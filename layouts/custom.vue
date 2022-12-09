515ceda2bbc96dc2159b9deeefd909bf76f032a6
Nicole <nicole929chan@gmail.com>
2022-12-09 15:46:29 +0800

    play around

---
+++ layouts/custom.vue
@@ -0,0 +1,9 @@
+<template>
+	<nav>
+		<h1>this is Custom layout</h1>
+		<NuxtLink to="/">Home</NuxtLink>
+		<main>
+			<slot />
+		</main>
+	</nav>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+	<nav>
+		<h1>this is Default layout</h1>
+		<NuxtLink to="/">Home</NuxtLink>
+		<main>
+			<slot />
+		</main>
+	</nav>
+</template>
